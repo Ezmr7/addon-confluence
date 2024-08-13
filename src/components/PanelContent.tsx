0ee40bf0a65b06987a9536f5dc05cd0106f6bc78
Josh Roberts <68209484+joshrobertsv2@users.noreply.github.com>
2024-08-13 12:18:29 -0500

    cleanup

--- src/components/PanelContent.tsx
+++ src/components/PanelContent.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParameter } from "@storybook/manager-api"
+import { useParameter } from "@storybook/manager-api";
 import { styled } from "@storybook/theming";
 import { Button } from "@storybook/components";
 
@@ -7,31 +7,32 @@ export const RequestDataButton = styled(Button)({
   marginTop: "1rem",
 });
 
-
 /**
  * Checkout https://github.com/storybookjs/storybook/blob/next/code/addons/jest/src/components/Panel.tsx
  * for a real world example
  */
 export const PanelContent = ({}) => {
-  
-  const [data, setData]= useState("<p style=\"color: white; font-size: 125%;\">Loading...</p>")
+  const [data, setData] = useState(
+    '<p style="color: white; font-size: 125%;">Loading...</p>',
+  );
 
-  const page = useParameter('confluence', {id: '0', domain: 'none'});
+  const page = useParameter("confluence", { id: "0", domain: "none" });
 
   useEffect(() => {
     const getData = async () => {
-      console.log("This be request:");
-      const response = await fetch(`/confluence?id=${page.id}&domain=${page.domain}`);
-      setData("<div style=\"color: white; font-size: 125%;\">" + await response.json() + "</div>");
-      console.log("This be response:", response);
-    }
+      const response = await fetch(
+        `/confluence?id=${page.id}&domain=${page.domain}`,
+      );
+      setData(
+        '<div style="color: white; font-size: 125%;">' +
+          (await response.json()) +
+          "</div>",
+      );
+    };
     getData();
-  }, [page])
+  }, [page]);
 
-  return (
-    <div id="pageFrame" dangerouslySetInnerHTML={{__html:data}}>
-    </div>
-  );
-}
+  return <div id="pageFrame" dangerouslySetInnerHTML={{ __html: data }}></div>;
+};
 
 // Old method for displaying addon data -> <iframe id="iframe" loading="lazy" height="100vh" width="100%" frameBorder="0"style={{overflow:"hidden", height:"100vh", width:"100%"}} srcDoc={data} title="Confluence Docs"></iframe>
--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const ADDON_ID = "storybook/addon-confluence";
+export const ADDON_ID = "addon-confluence";
 export const TOOL_ID = `${ADDON_ID}/tool`;
 export const PANEL_ID = `${ADDON_ID}/panel`;
 export const TAB_ID = `${ADDON_ID}/tab`;
