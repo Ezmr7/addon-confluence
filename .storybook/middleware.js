d794f05a2d0a7a3fa6ec940264d08c6c851cdc11
Ezekiel Mohr <ezekielmohr27@gmail.com>
2024-08-13 06:04:17 -0500

    Added confluence middleware as module

--- .storybook/middleware.js
+++ .storybook/middleware.js
@@ -1,4 +1,5 @@
 // middleware.js is the necessary file name for accessing the backend of Storybook, which gives the ability to proxy requests and avoid CORS blocking.
+
 require("dotenv").config();
 
 const CONFLUENCE_AUTHORIZATION = btoa(process.env.CONFLUENCE_EMAIL + ":" + process.env.CONFLUENCE_TOKEN);
@@ -14,12 +15,16 @@ const getConfluencePage = async (req, res, next) => {
         const url = `https://${domain}.atlassian.net/wiki/api/v2/pages/${id}?body-format=view`
         const response = await fetchPage(CONFLUENCE_AUTHORIZATION, url);
         const data = await response.json();
-        res.locals.page = data.body.view.value;
-        return next();
+        if (res) {
+          res.locals.page = data.body.view.value;
+          return next();
+        }
     } catch (error) {
-        // console.log('Error: In getConfluencePage middleware', error);
-        res.locals.page = "<p>No Confluence page found.</p>"
-        return next();
+        console.log('Error: In getConfluencePage middleware', error);
+        if (res.locals) {
+          res.locals.page = "<p>No Confluence page found.</p>"
+          return next();
+        }
     }
   }
 
@@ -28,3 +33,5 @@ module.exports = function expressMiddleware(router) {
         return res.status(200).json(res.locals.page);
     });
 };
+
+module.exports = getConfluencePage
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,34 @@
 // make it work with --isolatedModules
-export default {};
+require("dotenv").config();
+
+const CONFLUENCE_AUTHORIZATION = btoa(process.env.CONFLUENCE_EMAIL + ":" + process.env.CONFLUENCE_TOKEN);
+
+const fetchPage = async (auth:string, url:string) => {
+    const results = await fetch(url, {method: "GET", headers: {"Access-Control-Allow-Origin": "*", "Authorization": `Basic ${auth}`, "Content-Type": "application/json"}, mode: "cors"});
+    return results;
+  }
+  
+const getConfluencePage = async (req:any, res:any, next:any) => {
+    try{
+        const {id, domain} = req.query;
+        const url = `https://${domain}.atlassian.net/wiki/api/v2/pages/${id}?body-format=view`
+        const response = await fetchPage(CONFLUENCE_AUTHORIZATION, url);
+        const data = await response.json();
+        if (res) {
+            res.locals.page = data.body.view.value;
+            return next();
+        }
+    } catch (error) {
+        // console.log('Error: In getConfluencePage middleware', error);
+        if (res) {
+            res.locals.page = "<p>No Confluence page found.</p>"
+        }
+        return next();
+    }
+  }
+
+export const confluenceMiddleware = (router:any) => {
+    router.get("/confluence", getConfluencePage, (req:any, res:any) => {
+        return res.status(200).json(res.locals.page);
+    });
+};
