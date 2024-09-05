9476ba984e05a1912d4673aebb7f6d2cce7eeb80
Josh Roberts <68209484+joshrobertsv2@users.noreply.github.com>
2024-09-05 11:47:14 -0500

    reverting changes in preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,27 +1,4 @@
 import type { Preview } from "@storybook/react";
-import { DocsContainer } from "@storybook/addon-docs";
-import * as React from "react";
-import { PanelContent } from "../src/components/PanelContent";
-import { useParameter } from "@storybook/manager-api";
-
-const CustomDocsContainer: React.FC<any> = ({ children, ...rest }) => {
-  console.log(
-    "CustomDocsContainer",
-    rest.context?.componentStoriesValue[0]?.parameters?.confluence,
-  );
-  const page = useParameter("confluence", { id: null, domain: null });
-  return (
-    <React.Fragment>
-      <DocsContainer {...rest}>{children}</DocsContainer>
-      <div style={{ marginTop: "20px" }}>
-        {rest.context &&
-          rest.context?.componentStoriesValue[0]?.parameters?.confluence && (
-            <PanelContent /> // Render only if confluence parameters are present
-          )}
-      </div>
-    </React.Fragment>
-  );
-};
 
 const preview: Preview = {
   parameters: {
@@ -34,9 +11,6 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/,
       },
-    },
-    docs: {
-      container: CustomDocsContainer,
     },
   },
 };
